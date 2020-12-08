c9becc6b6a2032a863faae171bd8716e599d12cd
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-12-08 15:54:04 -0800

    users can log in

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const server = require('http').Server(app);
 // socket.io
 const io = require('socket.io')(server);
 io.on('connection', (socket) => {
+    require('./sockets/chat.js')(io, socket);
     console.log('🔌 New user connected! 🔌');
 });
 
--- public/index.js
+++ public/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", onReady);
+
+function onReady() {
     const socket = io.connect();
-});
+
+    document.getElementById('create-user-btn').addEventListener('click', (e) => {
+        e.preventDefault();
+        let username = document.getElementById('username-input').value;
+        if (username.length > 0) {
+            // Emit to server the new user
+            socket.emit('new user', username);
+            document.querySelector('.username-form').remove();
+        }
+    });
+
+    // socket listeners
+    socket.on('new user', (username) => {
+        console.log(`${username} has joined the chat! ✋`);
+    })
+}
