b62324beb8fcb0890c118bc98aa825cf0df4d14d
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-12-08 15:32:10 -0800

    integrated socket.io

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 
-// Socket.io has to use http server
+// socket.io has to use http server
 const server = require('http').Server(app);
 
+// socket.io
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log('🔌 New user connected! 🔌');
+});
+
 // Express View Engine for Handlebars
-const exphpbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, recs) => {
-    resizeBy.render('index.handlebars');
-})
+// Public Folder
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'});
+});
 
 server.listen('3000', () => {
     console.log('Server listening on port 3000! http://localhost:3000')
-})
+});
