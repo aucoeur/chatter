657f66c7a220a56d3b471cf7fec89d8d82289f30
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-12-08 16:59:04 -0800

    Users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -3,18 +3,62 @@ document.addEventListener("DOMContentLoaded", onReady);
 function onReady() {
     const socket = io.connect();
 
+    let currentUser;
+
     document.getElementById('create-user-btn').addEventListener('click', (e) => {
         e.preventDefault();
         let username = document.getElementById('username-input').value;
         if (username.length > 0) {
+            currentUser = username;
             // Emit to server the new user
             socket.emit('new user', username);
             document.querySelector('.username-form').remove();
+
+            const mainContainer = document.querySelector('.main-container');
+            mainContainer.style.display = 'flex';
+        }
+    });
+
+    document.getElementById('send-chat-btn').addEventListener('click', (e) => {
+        e.preventDefault();
+        let message = document.getElementById('chat-input').value;
+        if (message.length > 0) {
+            socket.emit('new message', {
+                sender: currentUser,
+                message: message
+            });
+            document.getElementById('chat-input').value = "";
         }
     });
 
     // socket listeners
     socket.on('new user', (username) => {
         console.log(`${username} has joined the chat! ✋`);
+        let user = document.createElement("div")
+        user.classList.add('user-online');
+        user.textContent = `${username}`;
+
+        const onlineUsers = document.querySelector('.users-online')
+        onlineUsers.appendChild(user);
+    });
+
+    socket.on('new message', (data) => {
+        const message =  document.createElement("div")
+        message.classList.add('message')
+
+        const msgUser = document.createElement("p");
+        msgUser.classList.add('message-user');
+        msgUser.textContent = `${data.sender}`;
+
+        const msgText = document.createElement("p");
+        msgText.classList.add('message-text');
+        msgText.textContent = `${data.message}`;
+
+        message.appendChild(msgUser);
+        message.appendChild(msgText);
+
+        let box = document.querySelector('.message-container')
+        box.appendChild(message);
+
     })
 }
--- sockets/chat.js
+++ sockets/chat.js
@@ -2,5 +2,12 @@ module.exports = (io, socket) => {
     socket.on('new user', (username) => {
         console.log(`${username} has joined the chat! ✋`);
         io.emit('new user', username);
+    });
+
+    //Listen for new messages
+    socket.on('new message', (data) => {
+        // Send that data back to ALL clients
+        console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+        io.emit('new message', data);
     })
 }
